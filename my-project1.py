a5c8f9de89406bd3fd4513eec0a70a41a720a1f3
Deshan989 <chamoddeshan68@gmail.com>
2023-10-13 14:50:20 +0530

    My first commit

---
+++ my-project1.py
@@ -0,0 +1,8 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def add_numbers(a, b):
+...     return a + b
+... num1 = float(input("Enter the first number: "))
+... num2 = float(input("Enter the second number: ")
+... result = add_numbers(num1, num2)
+... print("The sum of {} and {} is: {}".format(num1, num2, result))

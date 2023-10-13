4cc17049307b37d2f0e3aa6c009a5a1804ddbaaa
Deshan989 <chamoddeshan68@gmail.com>
2023-10-13 14:59:36 +0530

    My fourth commit

--- my-project1.py
+++ my-project1.py
@@ -1,10 +1,11 @@
 Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> def add_numbers(a, b, c, d):
-...     return a + b + c + d
+>>> def add_numbers(a, b, c, d, e):
+...     return a + b + c + d + e
 ... num1 = float(input("Enter the first number: "))
 ... num2 = float(input("Enter the second number: "))
 ... num3 = float(input("Enter the third number: "))
 ... num4 = float(input("Enter the fourth number: "))
-... result = add_numbers(num1, num2, num3, num4)
-... print("The sum of {}, {}, {}, and {} is: {}".format(num1, num2, num3, num4, result))
+... num5 = float(input("Enter the fifth number: "))
+... result = add_numbers(num1, num2, num3, num4, num5)
+... print("The sum of {}, {}, {}, {}, and {} is: {}".format(num1, num2, num3, num4, num5, result))

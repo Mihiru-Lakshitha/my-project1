1d545e5cac76e3b9e99c68de86e67104f47056b3
Mihiru-Lakshitha <mihiru141@gmail.com>
2023-10-13 16:16:02 +0530

    Final version

--- my-project1.py
+++ my-project1.py
@@ -1,10 +1,19 @@
-def add_numbers(a, b, c, d, e, f):
-    return a + b + c + d + e + f
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-num3 = float(input("Enter the third number: "))
-num4 = float(input("Enter the fourth number: "))
-num5 = float(input("Enter the fifth number: "))
-num6 = float(input("Enter the sixth number: "))
-result = add_numbers(num1, num2, num3, num4, num5, num6)
-print("The sum of {}, {}, {}, {}, {}, and {} is: {}".format(num1, num2, num3, num4, num5, num6, result))
+def add_numbers(*args):
+    return sum(args)
+
+# Get input from the user
+num_count = int(input("Enter the number of values you want to add: "))
+
+# Initialize an empty list to store the numbers
+numbers = []
+
+# Get the numbers from the user
+for i in range(num_count):
+    num = float(input(f"Enter number {i + 1}: "))
+    numbers.append(num)
+
+# Calculate the sum using the add_numbers function
+result = add_numbers(*numbers)
+
+# Print the result
+print("The sum of the numbers is:", result)

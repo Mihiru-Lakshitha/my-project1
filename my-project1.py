5fb356779925a4190dec06f68d6b207011aed72e
Deshan989 <chamoddeshan68@gmail.com>
2023-10-13 15:01:28 +0530

    My fifth commit

--- my-project1.py
+++ my-project1.py
@@ -1,11 +1,10 @@
-Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> def add_numbers(a, b, c, d, e):
-...     return a + b + c + d + e
-... num1 = float(input("Enter the first number: "))
-... num2 = float(input("Enter the second number: "))
-... num3 = float(input("Enter the third number: "))
-... num4 = float(input("Enter the fourth number: "))
-... num5 = float(input("Enter the fifth number: "))
-... result = add_numbers(num1, num2, num3, num4, num5)
-... print("The sum of {}, {}, {}, {}, and {} is: {}".format(num1, num2, num3, num4, num5, result))
+def add_numbers(a, b, c, d, e, f):
+    return a + b + c + d + e + f
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+num5 = float(input("Enter the fifth number: "))
+num6 = float(input("Enter the sixth number: "))
+result = add_numbers(num1, num2, num3, num4, num5, num6)
+print("The sum of {}, {}, {}, {}, {}, and {} is: {}".format(num1, num2, num3, num4, num5, num6, result))
